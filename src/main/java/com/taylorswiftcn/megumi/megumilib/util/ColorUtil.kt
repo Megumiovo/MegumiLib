0132929cb504ff17f1c5ea89ac328907c64a7d56
Wei_Boss <1731598625@qq.com>
2019-02-22 15:05:34 +0800

    [ADD] 字符串颜色处理

---
+++ src/main/java/com/taylorswiftcn/megumi/megumilib/util/ColorUtil.kt
@@ -0,0 +1,22 @@
+package com.taylorswiftcn.megumi.megumilib.util
+
+import java.util.ArrayList
+
+class ColorUtil {
+    companion object {
+
+        fun replace(_string: String): String {
+            return _string.replace("§", "&")
+        }
+
+        fun replace(_strings: List<String>): List<String> {
+            val strings = ArrayList<String>()
+
+            for (s in _strings) {
+                strings.add(replace(s))
+            }
+
+            return strings
+        }
+    }
+}
